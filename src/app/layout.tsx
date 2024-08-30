3dc0779b1ea61b98b920f32af94d64d77b19795c
Aditya Ghadge <adityaghadge360@gmail.com>
2024-08-30 16:11:45 +0530

    update website title and desc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import clsx from "clsx";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "AI Blogger",
+  description: "Generate blogs in seconds",
 };
 
 export default function RootLayout({
