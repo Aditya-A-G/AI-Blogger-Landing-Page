c7c867aafd2a84bdb0c86cc2e4c0abad34d5bf20
Aditya Ghadge <adityaghadge360@gmail.com>
2024-08-30 16:27:35 +0530

    create product showcase section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
+import { ProductShowcase } from "@/sections/ProductShowcase";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <ProductShowcase />
     </>
   );
 }
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,57 @@
+"use client";
+
+import productImage from "@/assets/product-image.png";
+import Image from "next/image";
+import pyramidImage from "@/assets/pyramid.png";
+import tubeImage from "@/assets/tube.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+
 export const ProductShowcase = () => {
-  return null;
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+  return (
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-clip"
+    >
+      <div className="container">
+        <div className="section-heading">
+          <div className="flex justify-center">
+            <div className="tag">Content Mastery</div>
+          </div>
+          <h2 className="section-title mt-5">
+            A more effective way to create blogs
+          </h2>
+          <p className="section-description mt-5">
+          Our AI-driven platform simplifies content creation, helping you generate professional-grade blogs and articles quickly. Discover the next generation of writing assistance today!
+          </p>
+        </div>
+        <div className="relative">
+          <Image src={productImage} alt="Product Image" className="mt-10" />
+          <motion.img
+            src={pyramidImage.src}
+            alt="Pyramid Image"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+            style={{translateY}}
+          />
+          <motion.img
+            src={tubeImage.src}
+            alt="Tube Image"
+            height={248}
+            width={248}
+            style={{translateY}}
+            className="hidden md:block absolute bottom-14 -left-36"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
